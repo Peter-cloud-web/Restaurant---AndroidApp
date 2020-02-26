707ebc00a695862514904ae6e05a896643dad61c
Peter-cloud-web <petergichia165@gmail.com>
2020-02-26 07:37:29 +0300

    Use toast to display a fading message when the button is clicked as an onClick action

--- app/src/main/java/com/example/myrestaurant/MainActivity.java
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //do something
+                Toast.makeText(MainActivity.this,"Welcome to manhattan", Toast.LENGTH_LONG).show();
             }
         });
 
