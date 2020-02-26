47f54e1a2200acc2e18f2db1d3a8fed98468667c
Peter-cloud-web <petergichia165@gmail.com>
2020-02-26 10:00:31 +0300

    use TAG to provide extra information to of the view and store data related to the views

--- app/src/main/java/com/example/myrestaurant/MainActivity.java
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.myrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.util.Log;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,location);
                 Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
 
             }
         });
